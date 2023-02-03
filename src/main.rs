2cece9b2eb4e7343a5e215c0ef39c8e88c892a42
Fitti <dev@fitti.io>
2023-02-03 05:37:30 +0100

    Add some function skeletons

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    let client = get_app_access_client();
+    let yvonnie_hours = client.get_total_hours("yvonnie");
+    let kkatamina_hours = client.get_total_hours("kkatamina");
+    println!("Yvonne:  {}h\nMiyoung: {}h", yvonnie_hours, kkatamina_hours);
+}
+
+fn get_app_access_client() -> TwitchClient {
+    let mut client = TwitchClient::new();
+    client.authenticate();
+    client
+}
+
+struct TwitchClient {
+    token: String,
+}
+
+impl TwitchClient {
+    fn new() -> TwitchClient {
+        TwitchClient {
+            token: "".to_owned(),
+        }
+    }
+
+    fn authenticate(&mut self) {
+        self.token = "".to_owned();
+    }
+
+    fn get_total_hours(&self, broadcaster_name: &str) -> u32 {
+        0
+    }
 }
