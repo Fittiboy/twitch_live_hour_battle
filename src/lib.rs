e403282928ddf949249aeab06a8d33a4b7ac306d
Fitti <dev@fitti.io>
2023-02-04 13:55:16 +0100

    Use more helpful function name

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ pub const OAUTH2_URL: &str = "https://id.twitch.tv/oauth2/token";
 pub const VIDEOS_URL: &str = "https://api.twitch.tv/helix/videos";
 pub const USERS_URL: &str = "https://api.twitch.tv/helix/users";
 
-pub fn get_app_access_client() -> TwitchClient {
+pub fn authenticated_twitch_client() -> TwitchClient {
     let mut client = TwitchClient::new();
     client.authenticate();
     client
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use twitch_live_hour_battle::get_app_access_client;
+use twitch_live_hour_battle::authenticated_twitch_client;
 
 fn main() {
-    let client = get_app_access_client();
+    let client = authenticated_twitch_client();
     let yvonnie_hours = client.total_hours("yvonnie");
     let kkatamina_hours = client.total_hours("kkatamina");
     println!("Yvonne:  {}h\nMiyoung: {}h", yvonnie_hours, kkatamina_hours);
